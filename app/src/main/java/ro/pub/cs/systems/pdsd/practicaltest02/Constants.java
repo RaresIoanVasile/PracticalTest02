c7546b47c96a86a7a413084c20b9fa45cc6b655f
RaresIoanVasile <101416587+RaresIoanVasile@users.noreply.github.com>
2023-05-25 17:33:33 +0300

    Update Constants.java

--- app/src/main/java/ro/pub/cs/systems/pdsd/practicaltest02/Constants.java
+++ app/src/main/java/ro/pub/cs/systems/pdsd/practicaltest02/Constants.java
@@ -5,7 +5,7 @@ public class Constants {
     final public static boolean DEBUG = true;
 
     final public static String WEB_SERVICE_API_KEY = "dbd3b02d8958d62185d02e944cd5f522";
-    final public static String WEB_SERVICE_ADDRESS = "https://api.dictionaryapi.dev/api/v2/entries/en/car";
+    final public static String WEB_SERVICE_ADDRESS = "https://api.dictionaryapi.dev/api/v2/entries/en/";
 
     final public static String UNITS = "metric";
     final public static String TEMPERATURE = "temperature";
