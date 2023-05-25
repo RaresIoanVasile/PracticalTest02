53d47bf72a4c29ef19ec1b8c29cf6e7a9f8d66e2
unknown <raresvasile59@gmail.com>
2023-05-25 17:08:54 +0300

    gata

---
+++ app/src/main/java/ro/pub/cs/systems/pdsd/practicaltest02/CommunicationThread.java
@@ -0,0 +1,115 @@
+package ro.pub.cs.systems.pdsd.practicaltest02;
+
+import android.util.Log;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.util.HashMap;
+
+import cz.msebera.android.httpclient.HttpEntity;
+import cz.msebera.android.httpclient.HttpResponse;
+import cz.msebera.android.httpclient.client.HttpClient;
+import cz.msebera.android.httpclient.client.methods.HttpGet;
+import cz.msebera.android.httpclient.impl.client.DefaultHttpClient;
+import cz.msebera.android.httpclient.util.EntityUtils;
+
+public class CommunicationThread extends Thread{
+    private final ServerThread serverThread;
+    private final Socket socket;
+
+    public CommunicationThread(ServerThread serverThread, Socket socket) {
+        this.serverThread = serverThread;
+        this.socket = socket;
+    }
+
+    @Override
+    public void run() {
+        // It first checks whether the socket is null, and if so, it logs an error and returns.
+        if (socket == null) {
+            Log.e(Constants.TAG, "[COMMUNICATION THREAD] Socket is null!");
+            return;
+        }
+        try {
+            // Create BufferedReader and PrintWriter instances for reading from and writing to the socket
+            BufferedReader bufferedReader = Utilities.getReader(socket);
+            PrintWriter printWriter = Utilities.getWriter(socket);
+            Log.i(Constants.TAG, "[COMMUNICATION THREAD] Waiting for parameters from client (city / information type!");
+
+            // Read the city and informationType values sent by the client
+            String word = bufferedReader.readLine();
+            if (word == null || word.isEmpty()) {
+                Log.e(Constants.TAG, "[COMMUNICATION THREAD] Error receiving parameters from client (city / information type!");
+                return;
+            }
+
+            // It checks whether the serverThread has already received the weather forecast information for the given city.
+            HashMap<String, Information> data = serverThread.getData();
+            Information information;
+            if (data.containsKey(word)) {
+                Log.i(Constants.TAG, "[COMMUNICATION THREAD] Getting the information from the cache...");
+                information = data.get(word);
+            } else {
+                Log.i(Constants.TAG, "[COMMUNICATION THREAD] Getting the information from the webservice...");
+                HttpClient httpClient = new DefaultHttpClient();
+                String pageSourceCode = "";
+
+                // make the HTTP request to the web service
+                HttpGet httpGet = new HttpGet(Constants.WEB_SERVICE_ADDRESS);
+                HttpResponse httpGetResponse = httpClient.execute(httpGet);
+                HttpEntity httpGetEntity = httpGetResponse.getEntity();
+                if (httpGetEntity != null) {
+                    pageSourceCode = EntityUtils.toString(httpGetEntity);
+                }
+                if (pageSourceCode == null) {
+                    Log.e(Constants.TAG, "[COMMUNICATION THREAD] Error getting the information from the webservice!");
+                    return;
+                } else Log.i(Constants.TAG, pageSourceCode);
+
+                // Parse the page source code into a JSONObject and extract the needed information
+                JSONArray content = new JSONArray(pageSourceCode);
+                JSONObject all = content.getJSONObject(0);
+                JSONArray meanings = all.getJSONArray("meanings");
+                JSONObject def = meanings.getJSONObject(0);
+                JSONArray deff = def.getJSONArray("definitions");
+                JSONObject defff = deff.getJSONObject(0);
+                String rezultat = defff.getString("definition");
+                System.out.println(content.toString());
+
+                // Create a WeatherForecastInformation object with the information extracted from the JSONObject
+                information = new Information(rezultat);
+
+                // Cache the information for the given city
+                serverThread.setData(word, information);
+            }
+
+            if (information == null) {
+                Log.e(Constants.TAG, "[COMMUNICATION THREAD] Weather Forecast Information is null!");
+                return;
+            }
+
+            // Send the result back to the client
+            printWriter.println(information.definition);
+            printWriter.flush();
+        } catch (IOException | JSONException ioException) {
+            Log.e(Constants.TAG, "[COMMUNICATION THREAD] An exception has occurred: " + ioException.getMessage());
+            if (Constants.DEBUG) {
+                ioException.printStackTrace();
+            }
+        } finally {
+            try {
+                socket.close();
+            } catch (IOException ioException) {
+                Log.e(Constants.TAG, "[COMMUNICATION THREAD] An exception has occurred: " + ioException.getMessage());
+                if (Constants.DEBUG) {
+                    ioException.printStackTrace();
+                }
+            }
+        }
+    }
+}
---
+++ app/src/main/java/ro/pub/cs/systems/pdsd/practicaltest02/Information.java
@@ -0,0 +1,9 @@
+package ro.pub.cs.systems.pdsd.practicaltest02;
+
+public class Information {
+    String definition;
+
+    public Information(String definition) {
+        this.definition = definition;
+    }
+}
